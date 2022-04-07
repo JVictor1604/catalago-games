01636307cf5950ca561a38f8ab433dd9afeff2c0
JVictor1604 <josevictor8569@gmail.com>
2022-04-06 23:43:48 -0300

    commit 06/04

--- src/controllers/jogocontrollers.js
+++ src/controllers/jogocontrollers.js
@@ -15,11 +15,90 @@ export const getIndex = async (req, res) => {
 
 export const getDetalhes = async (req, res) => {
   try {
-    
     const id = Number(req.params.id);
-    const game = await Jogos.findByPk(req.params.id)
+    const game = await Jogos.findByPk(req.params.id);
 
     res.render("detalhes.ejs", { game });
+  } catch (err) {
+    res.status(500).send({
+      err: err.message,
+    });
+  }
+};
+
+export const getCadastro = (req, res) => {
+  try {
+    res.render("cadastro.ejs");
+  } catch (err) {
+    res.status(500).send({
+      err: err.message,
+    });
+  }
+};
+
+export const getCreate = async (req, res) => {
+  try {
+    const newGame = req.body;
+
+    if (!newGame) {
+      return res.redirect("/cadastro");
+    }
+
+    await Jogos.create(newGame);
+
+    res.redirect("/");
+  } catch (err) {
+    res.status(500).send({
+      err: err.message,
+    });
+  }
+};
+
+export const getEditar = async (req, res) => {
+  try {
+    const jogo = await Jogos.findByPk(req.params.id);
+
+    res.status(200).render("editar.ejs", { jogo });
+  } catch (err) {
+    res.status(500).send({
+      err: err.message,
+    });
+  }
+};
+
+export const postEditar = async (req, res) => {
+  const { nome, imagem, descricao, preço, estoque, plataforma } = req.body;
+
+  try {
+    await Jogos.update(
+      {
+        nome: nome,
+        imagem: imagem,
+        descricao: descricao,
+        preço: preço,
+        estoque: estoque,
+        plataforma: plataforma,
+      },
+      {
+        where: { id: req.params.id },
+      }
+    );
+
+    res.status(200).redirect("/");
+  } catch (err) {
+    res.status(500).send({
+      err: err.message,
+    });
+  }
+};
+
+export const getDelete = async (req, res) => {
+  try {
+    await Jogos.destroy({
+      where: { id: req.params.id },
+    });
+
+    res.status(200).redirect("/");
   } catch (err) {
     res.status(500).send({
       err: err.message,
--- src/routes/router.js
+++ src/routes/router.js
@@ -1,8 +1,18 @@
 import express from "express";
-import { getDetalhes, getIndex } from "../controllers/jogocontrollers.js";
+import { getCadastro, getCreate, getDelete, getDetalhes, getEditar, getIndex, postEditar } from "../controllers/jogocontrollers.js";
 
 export const routes = express.Router();
 
 routes.get("/", getIndex);
 
 routes.get("/detalhes/:id", getDetalhes);
+
+routes.get("/cadastro", getCadastro);
+
+routes.post("/create", getCreate);
+
+routes.get("/editar/:id", getEditar);
+
+routes.post("/uptade/:id", postEditar);
+
+routes.get("/deletar/:id", getDelete);
